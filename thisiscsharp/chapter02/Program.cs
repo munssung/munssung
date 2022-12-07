cbca599715ee47203e3283945e6a249257a2a2e7
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2022-12-07 10:00:50 +0900

    Refector: Hello.cs [이것이 C#이다] 2장. 처음 만드는 C# 프로그램 예제 수행 (https://munssung.tistory.com/23)

--- thisiscsharp/chapter02/Program.cs
+++ thisiscsharp/chapter02/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+namespace Hello;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("사용법: Hello.exe <이름>");
+            return;
+        }
+        WriteLine("Hello, {0}!", args[0]);
+    }
+}
