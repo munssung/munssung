6279a98dd4a3074c61cf94b4144453d5d218fa14
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2022-12-08 10:09:12 +0900

    Add: chapter02/exercise1.cs 2장 연습 문제 코드 추가 (https://munssung.tistory.com/26)

---
+++ thisiscsharp/exercise/chapter02/exercise1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace exercise;
+
+class exercise1
+{
+    static void Main(string[] args)
+    {
+        WriteLine("여러분, 안녕하세요?");
+        WriteLine("반갑습니다!");
+    }
+}
