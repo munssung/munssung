a55a7bd69a993906d5b1ee8c815a18d65a0e558d
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2022-12-09 16:54:40 +0900

    Add: Decimal\Program.cs 이것이 C#이다. 3장 예제6 코드 작성 (https://munssung.tistory.com/27)

---
+++ thisiscsharp/example/chapter03/Decimal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace Decimal;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        float a = 3.1415_9265_3589_7932_3846_2643_3827_79f;
+        double b = 3.1415_9265_3589_7932_3846_2643_3827_79;
+        decimal c = 3.1415_9265_3589_7932_3846_2643_3827_79m;
+
+        WriteLine(a);
+        WriteLine(b);
+        WriteLine(c);
+    }
+}
