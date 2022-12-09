1b19872de04cad780f2bcee1a17b4ad22b5f1b87
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2022-12-09 16:40:46 +0900

    Add: FloatingPoint\Program.cs 이것이 C#이다. 3장 예제5 코드 작성 (https://munssung.tistory.com/27)

---
+++ thisiscsharp/example/chapter03/FloatingPoint/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace FloatingPoint;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        float a = 3.1415_9265_3589_7932_3846f;
+        WriteLine(a);
+
+        double b = 3.1415_9265_3589_7932_3846;
+        WriteLine(b);
+    }
+}
