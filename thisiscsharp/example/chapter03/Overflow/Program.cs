66abbf67f32a58488eeaf905d723798fb1370ca3
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2022-12-09 14:30:00 +0900

    Add: Overflow\Program.cs 이것이 C#이다. 3장 예제4 코드 작성 (https://munssung.tistory.com/27)

---
+++ thisiscsharp/example/chapter03/Overflow/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace Overflow;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        uint a = uint.MaxValue;
+        WriteLine(a);
+        a = a + 1;
+        WriteLine(a);
+    }
+}
