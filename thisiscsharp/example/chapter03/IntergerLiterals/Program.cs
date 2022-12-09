1b15d76c5010176edb33872cb8b797cc61995b81
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2022-12-09 09:56:07 +0900

    Add: IntergerLiterals\Program.cs 이것이 C#이다. 3장 예제2 코드 작성 (https://munssung.tistory.com/27)

--- thisiscsharp/example/chapter03/IntergerLiterals/Program.cs
+++ thisiscsharp/example/chapter03/IntergerLiterals/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+namespace IntergerLiterals;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        byte a = 240;
+        WriteLine($"a = {a}");
+
+        byte b = 0b1111_0000;
+        WriteLine($"b = {b}");
+
+        byte c = 0XF0;
+        WriteLine($"c = {c}");
+
+        uint d = 0x1234_abcd;
+        WriteLine($"d = {d}");
+    }
+}
