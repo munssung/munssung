1c85f389118ec726d37f2a70883f948effe19ccc
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2022-12-09 11:20:51 +0900

    Add: SignedUnsigned\Program.cs 이것이 C#이다. 3장 예제3 코드 작성 (https://munssung.tistory.com/27)

---
+++ thisiscsharp/example/chapter03/SignedUnsigned/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace SignedUnsigned;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        byte a = 255;
+        sbyte b = (sbyte)a;
+
+        WriteLine(a);
+        WriteLine(b);
+    }
+}
