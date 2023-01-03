55974ebce0ba60925e505d7283d2a636065b87cf
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-01-03 09:52:03 +0900

    Add: Char\Program.cs 이것이 C#이다. 3장 예제7 코드 작성 (https://munssung.tistory.com/27)

---
+++ thisiscsharp/example/chapter03/Char/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace Char;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        char a = '안';
+        char b = '녕';
+        char c = '하';
+        char d = '세';
+        char e = '요';
+
+        Write(a);
+        Write(b);
+        Write(c);
+        Write(d);
+        Write(e);
+        WriteLine();
+        WriteLine();
+    }
+}
