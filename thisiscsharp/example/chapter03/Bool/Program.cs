ed78130dfbae70563f357fa87d270f0538598a5a
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-01-03 10:36:24 +0900

    Add: Bool\Program.cs 이것이 C#이다. 3장 예제7 코드 작성 (https://munssung.tistory.com/27)

---
+++ thisiscsharp/example/chapter03/Bool/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace Bool;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        bool a = true;
+        bool b = false;
+
+        WriteLine(a);
+        WriteLine(b);
+    }
+}
