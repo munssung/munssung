6fb6f3b844a77943d3a5f3711d459f9a952038b6
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-01-05 10:38:29 +0900

    Add: FloatConversion\Program.cs 이것이 C#이다. 3장 예제12 코드 작성 (https://munssung.tistory.com/31)

---
+++ thisiscsharp/example/chapter03/FloatConversion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace FloatConversion;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        float a = 69.6875f;
+        WriteLine(a);
+
+        double b = (double)a;
+        WriteLine("b : {0}",b);
+        WriteLine("69.6875 == b : {0}", 69.6875 == b);
+
+        float x = 0.1f;
+        WriteLine("x : {0}",x);
+
+        double y = (double)x;
+        WriteLine("t : {0}",y);
+
+        WriteLine("0.1 == y : {0}",0.1 == y);
+    }
+}
