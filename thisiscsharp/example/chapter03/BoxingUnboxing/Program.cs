eba68d5b196bdaa1cccdbd3d12b96227fd46278d
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-01-05 09:19:57 +0900

    Add: BoxingUnboxing\Program.cs 이것이 C#이다. 3장 예제10 코드 작성 (https://munssung.tistory.com/27)

---
+++ thisiscsharp/example/chapter03/BoxingUnboxing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace BoxingUnboxing;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        int a = 123;
+        object b = (object)a;
+        int c = (int)b;
+
+        WriteLine(a);
+        WriteLine(b);
+        WriteLine(c);
+
+        double x = 3.1414213;
+        object y = x;
+        double z = (double)y;
+
+        WriteLine(x);
+        WriteLine(y);
+        WriteLine(z);
+    }
+}
