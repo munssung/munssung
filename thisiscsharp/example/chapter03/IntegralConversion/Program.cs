9fad52400dfddfa66bd9dd61b2994a0022243d8c
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-01-05 09:53:39 +0900

    Add: BoxingUnboxing\Program.cs 이것이 C#이다. 3장 예제11 코드 작성 (https://munssung.tistory.com/31)

---
+++ thisiscsharp/example/chapter03/IntegralConversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace IntegralConversion;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        sbyte a = 127;
+        WriteLine(a);
+
+        int b = (int)a;
+        WriteLine(b);
+
+        int x = 128;
+        WriteLine(x);
+
+        sbyte y = (sbyte)x;
+        WriteLine(y);
+    }
+}
