e75bddc5016d10adeaef78d089fb95a670d64ff3
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-02-06 10:09:47 +0900

    Add: FloatToInteger\Program.cs 이것이 C#이다. 3장 예제14 코드 작성 (https://munssung.tistory.com/31)

---
+++ thisiscsharp/example/chapter03/FloatToInteger/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace FloatToInteger;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        float a = 0.9f;
+        int b = (int)a;
+        WriteLine(b);
+
+        float c = 1.1f;
+        int d = (int)c;
+        WriteLine(d);
+    }
+}
