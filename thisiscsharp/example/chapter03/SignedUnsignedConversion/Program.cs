7a727a31af700dd6306b1aa4bfe265d9876f6ce3
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-02-06 09:59:54 +0900

    Add: SignedUnsignedConversion\Program.cs 이것이 C#이다. 3장 예제13코드 작성 (https://munssung.tistory.com/31)

---
+++ thisiscsharp/example/chapter03/SignedUnsignedConversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace SignedUnsignedConversion;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        int a = 500;
+        WriteLine(a);
+
+        uint b = (uint)a;
+        WriteLine(b);
+
+        int x = -30;
+        WriteLine(x);
+
+        uint y = (uint)x;
+        WriteLine(y);
+    }
+}
