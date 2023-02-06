5a35757ff77bf5dfc6ca50b6c93f0e30bce24e3d
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-02-06 17:00:46 +0900

    Add: Constant\Program.cs 이것이 C#이다. 3장 예제16 코드 작성 (https://munssung.tistory.com/31)

---
+++ thisiscsharp/example/chapter03/Constant/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace Constant;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        const int MAX_INT = 2147483647;
+        const int MIN_INT = -2147483648;
+
+        WriteLine(MAX_INT);
+        WriteLine(MIN_INT);
+    }
+}
