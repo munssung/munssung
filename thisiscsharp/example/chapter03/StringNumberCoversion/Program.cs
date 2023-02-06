e6209ab88e0b1475350ca0698f8a04c22111a6fa
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-02-06 16:48:42 +0900

    Add: StringNumberCoversion\Program.cs 이것이 C#이다. 3장 예제15 코드 작성 (https://munssung.tistory.com/31)

---
+++ thisiscsharp/example/chapter03/StringNumberCoversion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace StringNumberCoversion;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        int a = 123;
+        string b = a.ToString();
+        WriteLine(b);
+
+        float c = 3.14f;
+        string d = c.ToString();
+        WriteLine(d);
+
+        string e = "123456";
+        int f = Convert.ToInt32(e);
+        WriteLine(f);
+
+        string g = "1.2345";
+        float h = float.Parse(g);
+        WriteLine(h);
+    }
+}
