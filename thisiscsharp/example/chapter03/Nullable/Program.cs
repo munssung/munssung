cd3be7ded3e34efcdfc5c698b0f32ac6bff170ff
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-02-07 12:17:53 +0900

    Refector: Nullable\Program.cs 이것이 C#이다. 3장 예제20 코드 수정

--- thisiscsharp/example/chapter03/Nullable/Program.cs
+++ thisiscsharp/example/chapter03/Nullable/Program.cs
@@ -9,13 +9,13 @@ class MainApp
     {
         int? a = null;
 
-        WriteLine(a.HasValue);
+        WriteLine(a.HasValue); // a는 null이므로 False 출력
         WriteLine(a != null);
 
         a = 3;
 
-        WriteLine(a.HasValue);
+        WriteLine(a.HasValue); // a는 3을 갖고 있으므로 True 출력
         WriteLine(a != null);
-        WriteLine(a.Value);
+        WriteLine(a.Value); // 3을 출력
     }
 }
