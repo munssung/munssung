73cdd602a938543c46c4c267b8bbea153e787eb6
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-02-07 12:15:00 +0900

    Add: Nullable\Program.cs 이것이 C#이다. 3장 예제20 코드 작성 (https://munssung.tistory.com/31)

---
+++ thisiscsharp/example/chapter03/Nullable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Nullable;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        int? a = null;
+
+        WriteLine(a.HasValue);
+        WriteLine(a != null);
+
+        a = 3;
+
+        WriteLine(a.HasValue);
+        WriteLine(a != null);
+        WriteLine(a.Value);
+    }
+}
