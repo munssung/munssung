7ff4a7b25b80abc5df2d7a4ddf7ff93ff30fa54c
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-02-07 11:23:41 +0900

    Add: Enum2\Program.cs 이것이 C#이다. 3장 예제18 코드 작성 (https://munssung.tistory.com/31)

---
+++ thisiscsharp/example/chapter03/Enum2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace Enum2;
+
+class MainApp
+{
+    enum DialogResult { YES, NO, CANCLE, CONFIRM, OK }
+    static void Main(string[] args)
+    {
+        DialogResult result = DialogResult.YES;
+
+        WriteLine(result == DialogResult.YES);
+        WriteLine(result == DialogResult.NO);
+        WriteLine(result == DialogResult.CANCLE);
+        WriteLine(result == DialogResult.CONFIRM);
+        WriteLine(result == DialogResult.OK);
+    }
+}
