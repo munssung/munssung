fb8bc828a00ef2487ee99abad6c515043435a6e8
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-02-07 11:17:21 +0900

    Add: Enum\Program.cs 이것이 C#이다. 3장 예제17 코드 작성 (https://munssung.tistory.com/31)

---
+++ thisiscsharp/example/chapter03/Enum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace Enum;
+
+class MainApp
+{
+    enum DialogResult { YES, NO, CANCLE, CONFIRM, OK }
+    static void Main(string[] args)
+    {
+        WriteLine((int)DialogResult.YES);
+        WriteLine((int)DialogResult.NO);
+        WriteLine((int)DialogResult.CANCLE);
+        WriteLine((int)DialogResult.CONFIRM);
+        WriteLine((int)DialogResult.OK);
+    }
+}
