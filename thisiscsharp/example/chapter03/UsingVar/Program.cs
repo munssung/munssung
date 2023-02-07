be44395f0498b08bc0a030691751cd7b126ca33c
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-02-07 12:37:09 +0900

    Add: UsingVar\Program.cs 이것이 C#이다. 3장 예제21 코드 작성 (https://munssung.tistory.com/31)

---
+++ thisiscsharp/example/chapter03/UsingVar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace UsingVar;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        var a = 20;
+        WriteLine("Type: {0}, Value: {1}", a.GetType(), a);
+
+        var b = 3.1414213;
+        WriteLine("Type: {0}, Value: {1}", b.GetType(), b);
+
+        var c = "Hello, World!";
+        WriteLine("Type: {0}, Value: {1}", c.GetType(), c);
+
+        var d = new int[] { 10, 20, 30 };
+        Write("Type: {0}, Value: ",d.GetType());
+        foreach (var e in d)
+            Write("{0} ", e);
+
+        WriteLine();
+    }
+}
