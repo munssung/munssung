2cf5393850b2a762630858fec6ecca3c73b15cc5
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-03-27 15:50:19 +0900

    Add: ArithmaticOperators/Program.cs 이것이 C#이다. 4장 예제1 코드 작성 (https://munssung.tistory.com/36)

---
+++ thisiscsharp/example/chapter04/ArithmaticOperators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace ArithmaticOperators;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        int a = 111 + 222;
+        WriteLine($"a : {a}");
+
+        int b = a - 100;
+        WriteLine($"b : {b}");
+
+        int c = b * 10;
+        WriteLine($"c : {c}");
+
+        double d = c / 6.3;
+        WriteLine($"d : {d}");
+
+        WriteLine($"22 / 7 = {22 / 7}({22 % 7})");
+    }
+}
