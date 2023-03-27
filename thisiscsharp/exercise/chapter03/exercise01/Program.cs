d180ee1b2227d72670567e2b3fa07e39a0051c04
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-03-27 11:57:47 +0900

    Add: exercise01\Program.cs 이것이 C#이다. 연습문제 1 코드 작성 (https://munssung.tistory.com/35)

---
+++ thisiscsharp/exercise/chapter03/exercise01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercise01
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("사각형의 너비를 입력하세요.");
+            string width = Console.ReadLine();
+
+            Console.WriteLine("사각형의 높이를 입력하세요.");
+            string height = Console.ReadLine();
+
+            // 이곳에 사각형의 넓이를 계산하고
+            // 출력하는 루틴을 추가하세요
+            
+            int area = int.Parse(width)*int.Parse(height);
+            Console.WriteLine($"사각형의 넓이는 : {area}");
+        }
+    }
+}
