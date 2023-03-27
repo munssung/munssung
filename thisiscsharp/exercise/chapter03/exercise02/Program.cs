d2647a87b7ed33d5a7511051bb001f611725b216
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-03-27 13:40:13 +0900

    Add: exercise02\Program.cs 이것이 C#이다. 연습문제 2 코드 작성 (https://munssung.tistory.com/35)

---
+++ thisiscsharp/exercise/chapter03/exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace exercise02
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            var a = 2020;
+            var b = "double";
+
+            WriteLine(a.GetTypeCode());
+            WriteLine(b.GetTypeCode());
+        }
+    }
+}
