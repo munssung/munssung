d007e02c523748a5687dcc88e14b399fc7af545e
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-03-27 16:55:20 +0900

    Add: IncDecOperator/Program.cs 이것이 C#이다. 4장 예제2 코드 작성 (https://munssung.tistory.com/36)

---
+++ thisiscsharp/example/chapter04/IncDecOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace IncDecOperator;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        WriteLine(a++);
+        WriteLine(++a);
+
+        WriteLine(a--);
+        WriteLine(--a);
+    }
+}
