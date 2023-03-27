9926a7284b0d96908de46ac62ef6888368072ce3
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-03-27 11:25:13 +0900

    Add: StringFormatDateTime\Program.cs 이것이 C#이다. 3장 예제28 코드 작성 (https://munssung.tistory.com/32)

---
+++ thisiscsharp/example/chapter03/StringFormatDateTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using static System.Console;
+
+namespace StringFormatDateTime;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        DateTime dt = new DateTime(2018, 11, 3, 23, 18, 22);
+
+        WriteLine("12시간 형식: {0:yyyy-mm-dd tt hh:mm:ss (ddd)}", dt);
+        WriteLine("24시간 형식: {0:yyyy-mm-dd HH:mm:ss (dddd)}", dt);
+
+        CultureInfo ciKo = new CultureInfo("ko-KR");
+        WriteLine();
+        WriteLine(dt.ToString("yyyy-mm-dd tt hh:mm:ss (ddd)", ciKo));
+        WriteLine(dt.ToString("yyyy-mm-ss HH:mm:ss (dddd)", ciKo));
+        WriteLine(dt.ToString(ciKo));
+
+        CultureInfo ciEn = new CultureInfo("en-US");
+        WriteLine();
+        WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss {ddd)", ciEn));
+        WriteLine(dt.ToString("yyyy-MM-dd HH:mm:ss (dddd)", ciEn));
+        WriteLine(dt.ToString(ciEn));
+    }
+}
