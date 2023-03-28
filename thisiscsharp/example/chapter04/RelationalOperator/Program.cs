c383a94258c2cdd8e88bdb38c225f7c52b75f81d
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-03-28 10:16:11 +0900

    Add: RelationalOperator/Program.cs 이것이 C#이다. 4장 예제4 코드 작성 (https://munssung.tistory.com/36)

---
+++ thisiscsharp/example/chapter04/RelationalOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace RelationalOperator;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        WriteLine($"3 > 4 : {3 > 4}");
+        WriteLine($"3 >= 4 : {3 >= 4}");
+        WriteLine($"3 < 4 : {3 < 4}");
+        WriteLine($"3 <= 4 : {3 <= 4}");
+        WriteLine($"3 == 4 : {3 == 4}");
+        WriteLine($"3 != 4 : {3 != 4}");
+    }
+}
