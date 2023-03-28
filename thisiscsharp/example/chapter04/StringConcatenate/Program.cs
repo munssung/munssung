6e6ab95c69ea3caadfbb5a7aa562c796848636b8
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-03-28 09:38:02 +0900

    Add: StringConcatenate/Program.cs 이것이 C#이다. 4장 예제3 코드 작성 (https://munssung.tistory.com/36)

---
+++ thisiscsharp/example/chapter04/StringConcatenate/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace StringConcatenate;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        string result = "123" + "456";
+        WriteLine(result);
+
+        result = "Hello" + " " + "Wrold!";
+        WriteLine(result);
+    }
+}
