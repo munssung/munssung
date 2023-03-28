46fe5cfec3666398d0e28029bb2402289b3109a9
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-03-28 10:59:14 +0900

    Add: LogicalOperator/Program.cs 이것이 C#이다. 4장 예제4 코드 작성 (https://munssung.tistory.com/36)

---
+++ thisiscsharp/example/chapter04/LogicalOperator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace LogicalOperator;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        WriteLine("Testing && ...");
+        WriteLine($"1 > 00 && 4 < 5 : {1 > 00 && 4 < 5}");
+        WriteLine($"1 > 00 && 4 > 5 : {1 > 00 && 4 > 5}");
+        WriteLine($"1 == 00 && 4 > 5 : {1 == 00 && 4 > 5}");
+        WriteLine($"1 == 00 && 4 < 5 : {1 == 00 && 4 < 5}");
+
+        WriteLine("\nTesting || ...");
+        WriteLine($"1 > 00 || 4 < 5 : {1 > 00 || 4 < 5}");
+        WriteLine($"1 > 00 || 4 > 5 : {1 > 00 || 4 > 5}");
+        WriteLine($"1 == 00 || 4 > 5 : {1 == 00 || 4 > 5}");
+        WriteLine($"1 == 00 || 4 < 5 : {1 == 00 || 4 < 5}");
+
+        WriteLine("\nTesting ! ...");
+        WriteLine($"!True : {!true}");
+        WriteLine($"!False : {!false}");
+    }
+}
