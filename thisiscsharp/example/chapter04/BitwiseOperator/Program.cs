0b848c5d37598604e80cd1719f9271e0cc44ad58
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-03-28 16:25:43 +0900

    Add: BitwiseOperator/Program.cs 이것이 C#이다. 4장 예제9 코드 작성 (https://munssung.tistory.com/36)

---
+++ thisiscsharp/example/chapter04/BitwiseOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace BitwiseOperator;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        int a = 9;
+        int b = 10;
+
+        WriteLine($"{a} & {b} : {a & b}");
+        WriteLine($"{a} | {b} : {a | b}");
+        WriteLine($"{a} ^ {b} : {a ^ b}");
+
+        int c = 255;
+        WriteLine("~{0}(0x{0:X8}) : {1}0x{1:X8})", c, ~c);
+    }
+}
