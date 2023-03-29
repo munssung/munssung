acbac04d92a719341390ff3c5bd0aeab84109e53
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-03-29 14:13:15 +0900

    Add: Switch2/Program.cs 이것이 C#이다. 5장 예제4 코드 작성 (https://munssung.tistory.com/39)

---
+++ thisiscsharp/example/chapter05/Switch2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace Switch2;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        object obj = null;
+
+        string s = ReadLine();
+        if (int.TryParse(s, out int out_i))
+            obj = out_i;
+        else if (float.TryParse(s, out float out_f))
+            obj = out_f;
+        else
+            obj = s;
+
+        switch (obj)
+        {
+            case int i:
+                WriteLine($"{i}는 int 형식입니다.");
+                break;
+            case float f:
+                WriteLine($"{f}는 float 형식입니다.");
+                break;
+            default:
+                WriteLine($"{obj}(은)는 모르는 형식입니다.");
+                break ;
+        }
+    }
+}
