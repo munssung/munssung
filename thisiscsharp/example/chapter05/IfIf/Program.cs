6348242c48345581280da20794cd5449af42f907
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-03-29 11:46:05 +0900

    Add: IfIf/Program.cs 이것이 C#이다. 5장 예제1 코드 작성 (https://munssung.tistory.com/39)

---
+++ thisiscsharp/example/chapter05/IfIf/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace IfIf;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        Write("숫자를 입력하세요.  :  ");
+
+        string input = ReadLine();
+        int number = Convert.ToInt32(input);
+
+        if (number > 0)
+        {
+            if (number % 2 == 0)
+                WriteLine("0보다 큰 짝수");
+
+            else
+                WriteLine("0보다 큰 홀수");
+        }
+        else
+        {
+            WriteLine("0보다 작거나 같은 수");
+        }      
+        
+    }
+}
