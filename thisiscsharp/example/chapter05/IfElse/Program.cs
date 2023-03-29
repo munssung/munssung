9d223c2c1b20a4b6cce9760d0ede62b4d0434d3c
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-03-29 11:20:15 +0900

    Add: IfElse/Program.cs 이것이 C#이다. 5장 예제1 코드 작성 (https://munssung.tistory.com/39)

---
+++ thisiscsharp/example/chapter05/IfElse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace IfElse;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        Write("숫자를 입력하세요.  :  ");
+
+        string input = ReadLine();
+        int number = Int32.Parse(input);
+
+        if (number < 0)
+            WriteLine("음수");
+        else if (number > 0)
+            WriteLine("양수");
+        else
+            WriteLine("0");
+
+        if (number % 2 == 0)  // 2로 나누었을 때 나머지가 0
+            WriteLine("짝수");
+        else
+            WriteLine("홀수");
+    }
+}
