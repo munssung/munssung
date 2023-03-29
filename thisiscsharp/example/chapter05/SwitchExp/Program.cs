b38125acacffa578e9896faf37c935ecb796579a
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-03-29 14:54:21 +0900

    Add: SwitchExp/Program.cs 이것이 C#이다. 5장 예제5 코드 작성 (https://munssung.tistory.com/39)

---
+++ thisiscsharp/example/chapter05/SwitchExp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace SwitchExp;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        WriteLine("점수를 입력하세요");
+        int score = Convert.ToInt32(ReadLine());
+
+        WriteLine("재수강인가요? (y/n)");
+        string line = ReadLine();
+        bool repeated = line == "y" ? true : false;
+
+        string grade = (int)(Math.Truncate(score / 10.0) * 10) switch
+        {
+            90 when repeated == true => "B+",
+            90 => "A",
+            80 => "B",
+            70 => "C",
+            60 => "D",
+            _ => "F"
+        };
+
+        WriteLine($"학점 : {grade}");
+    }
+}
