a543aaa072054004f249b783f408a319f38c339c
DESKTOP-UQJMCLC\se.moon <munsung89@gmail.com>
2023-03-30 10:03:34 +0900

    Add: DoWhile/Program.cs 이것이 C#이다. 5장 예제7 코드 작성 (https://munssung.tistory.com/40)

---
+++ thisiscsharp/example/chapter05/DoWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace DoWhile;
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        int i = 10;
+
+        do
+        {
+            WriteLine("a) i : {0}", i--);
+            //WriteLine($"a) i : {i--}");
+        }
+        while (i>0);
+
+        do
+        {
+            WriteLine("b) i : {0}", i--);
+        }
+        while(i>0);
+    }
+}
